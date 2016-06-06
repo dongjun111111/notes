121ac42c24a71d911c49b11865e1555e38094d51
dongjun111111 <903456967@qq.com>
2016-06-06 08:18:56 +0800

    php rewrite

--- Golang/Golang表单输入DEMO/login.go
+++ Golang/Golang表单输入DEMO/login.go
@@ -1,44 +1,43 @@
 package main
 
 import (
-    "fmt"
-    "html/template"
-    "log"
-    "net/http"
-    "strings"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()      
-    
-    fmt.Println(r.Form)
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello astaxie!")
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
 }
 
- func login(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("method:", r.Method) 
-    if r.Method == "GET" {
-        t, _ := template.ParseFiles("login.gtpl")
-        t.Execute(w, nil)
-    } else {
-        r.ParseForm()
-        fmt.Println("username:", r.Form["username"])
-        fmt.Println("password:", r.Form["password"])
-    }
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
 }
 
 func main() {
-    http.HandleFunc("/", sayhelloName)       
-    http.HandleFunc("/login", login)         
-    err := http.ListenAndServe(":9090", nil) 
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
