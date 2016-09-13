fa1a3adf6316d3684007832f12e6b0f67ef60f07
dongjun111111 <903456967@qq.com>
2016-09-13 22:53:51 +0800

    响应式

---
+++ 前端/响应式单页面示例/zsy_files/badge_code_v2.js
@@ -0,0 +1,33 @@
+var b_txt = '';
+
+// write the badge
+	
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image1"><a href="#"><img src="./zsy_files/4873437116_a66ffaa9f5_s.jpg" alt="Flickr 上的一張相片" title="POGO" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image2"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="WebDesign #136 Magazine Cover" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image3"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="koi" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image4"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="abstract phoenix" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image5"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="abstract peacock" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image6"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="flow" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image7"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="phoenix" height="75" width="75"></a></div>';
+	
+		 	 		
+	b_txt+= '<div class="flickr_badge_image" id="flickr_badge_image8"><a href="#"><img src="./zsy_files/4872827433_003ef3523d_s.jpg" alt="Flickr 上的一張相片" title="bamboo" height="75" width="75"></a></div>';
+
+
+b_txt += '<span style="position:absolute;left:-999em;top:-999em;visibility:hidden" class="flickr_badge_beacon"><img src="" width="0" height="0" alt="" /></span>';
+
+document.write(b_txt);
