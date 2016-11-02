5c6210d1a71417f1496f22d6b329f65f8b0ae34b
dongjun111111 <903456967@qq.com>
2016-11-02 19:12:14 +0800

    sh

---
+++ Golang/监控程序脚本（程序退出自动重启）.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+PRO_NAME="程序名称" 
+while true;
+do
+NUM=`ps aux | grep PRO_NAME | grep -v grep |wc -l`
+if [ "${NUM}" -lt 1 ]
+then
+echo "${PRO_NAME} was killed"
+cd /home/go/src/程序名称         /*程序所在目录*/
+./程序名称                       /*启动程序*/
+else
+echo "ok"
+fi
+sleep 3 
+done
+
