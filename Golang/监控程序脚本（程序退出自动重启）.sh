929aa784d552ccdced3846ec03e20b67854e3014
Jason <903456967@qq.com>
2016-11-17 21:50:31 +0800

    bash

--- Golang/监控程序脚本（程序退出自动重启）.sh
+++ Golang/监控程序脚本（程序退出自动重启）.sh
@@ -6,8 +6,8 @@ NUM=`ps aux | grep PRO_NAME | grep -v grep |wc -l`
 if [ "${NUM}" -lt 1 ]
 then
 echo "${PRO_NAME} was killed"
-cd /home/go/src/程序名称         /*程序所在目录*/
-./程序名称                       /*启动程序*/
+cd /home/go/src/程序名称          /*程序所在目录*/
+./程序名称                        /*启动程序*/
 else
 echo "ok"
 fi
