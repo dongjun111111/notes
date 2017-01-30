4fb95dcffec16ba5c6bf6b334493947195e690a9
Jason <903456967@qq.com>
2017-01-31 00:04:57 +0800

    downloa

--- Golang/下载文件.go
+++ Golang/下载文件.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	urls := "http://www.fengyun5.com/Sibao/600/1.html"
 	res, _ := http.Get(urls)
-	file, _ := os.Create("download_files.html")
+	file, _ := os.Create("downloadFiles.html")
 	io.Copy(file, res.Body)
 	fmt.Println("下载完成！")
 }
