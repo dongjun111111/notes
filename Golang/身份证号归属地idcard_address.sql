8e9f45416f222761c717fc40aabf0f3744bde12e
Jason <903456967@qq.com>
2017-04-04 22:05:34 +0800

    G

--- Golang/身份证号归属地idcard_address.sql
+++ Golang/身份证号归属地idcard_address.sql
@@ -3,8 +3,8 @@ Navicat MySQL Data Transfer
 
 Source Server         : 身份证号归属地
 Source Server Version : 50616
-Source Host           : HaHaHa
-Source Database       : HaHaHa
+Source Host           : Ha
+Source Database       : Ha
 
 Target Server Type    : MYSQL
 Target Server Version : 50616
